0e5ca2966b1c68a67016cc21b650ed29b5fd2541
Jakub <jakuboleszczuk@protonmail.com>
2025-01-25 20:49:47 +0100

    add functions to count spaces in a sentence, find the longest word, and write user input to a file until "koniec" is entered

--- cw_podstawy_progr/poprawa.cpp
+++ cw_podstawy_progr/poprawa.cpp
@@ -4,6 +4,8 @@
 // 3.funkcja która policzy spacje w zdaniu
 // 4. funkcja która wypisze liczby pierwsze
 // 5. funkcja która wypisze do pliku liczby nie parzyste większe od 50
+// 6. Funkcja ktora znajdzie w stringu podanym przez uzytkowanika, spacje i napisze na ktorym sa miejscu
+// 7. Funkcja ktora tworzy plik i dopisuje w nim tekst wprowadzony przez uzytkownika do momentu wpisania "koniec"
 
 #include <iostream>
 #include <fstream>
@@ -37,13 +39,19 @@ int zad2()
 
 void zad3()
 {
-    string zdanie = "Ala ma kota a kot ma Ale";
+    fstream plik;
+    plik.open("poprawa.cpp", ios::in);
+    string linia;
+
     int licznik = 0;
-    for (int i = 0; i < zdanie.length(); i++)
+    while (getline(plik, linia))
     {
-        if (zdanie[i] == ' ')
+        for (int i = 0; i < linia.length(); i++)
         {
-            licznik++;
+            if (linia[i] == ' ')
+            {
+                licznik++;
+            }
         }
     }
     cout << licznik;
@@ -80,6 +88,103 @@ void zad5()
             plik << i << endl;
         }
     }
+    plik.close();
+}
+
+void zad6()
+{
+    string zdanie;
+    cout << "Podaj zdanie: ";
+    getline(cin, zdanie);
+    for (int i = 0; i < zdanie.length(); i++)
+    {
+        if (zdanie[i] == ' ')
+        {
+            cout << "Spacja na miejscu: " << i << endl;
+        }
+    }
+}
+// 7. Funkcja ktora tworzy plik i dopisuje w nim tekst wprowadzony przez uzytkownika do momentu wpisania "koniec"
+void zad7()
+{
+    fstream plik;
+    plik.open("tekst.txt", ios::app);
+    string tekst;
+    while (true)
+    {
+        cout << "Podaj tekst: ";
+        getline(cin, tekst);
+        if (tekst == "koniec")
+        {
+            break;
+        }
+        plik << tekst << endl;
+    }
+    plik.close();
+}
+
+// 8. Funkcja ktora odczyta z pliku slowa i policzy ktore jest najdluzsze
+void zad8()
+{
+    fstream plik;
+    plik.open("poprawa.cpp", ios::in);
+    string linia;
+    string slowo;
+    string najdluzsze;
+    int dlugosc = 0;
+    int najdluzszaDlugosc = 0;
+    while (getline(plik, linia))
+    {
+        for (int i = 0; i < linia.length(); i++)
+        {
+            if (linia[i] != ' ')
+            {
+                slowo += linia[i];
+            }
+            else
+            {
+                dlugosc = slowo.length();
+                if (dlugosc > najdluzszaDlugosc)
+                {
+                    najdluzszaDlugosc = dlugosc;
+                    najdluzsze = slowo;
+                }
+                slowo = "";
+            }
+        }
+    }
+    cout << "Najdluzsze slowo: " << najdluzsze << endl;
+    plik.close();
+}
+
+// 9. Funkcja ktora odczyta z pliku liczby i zapisze je do tablicy, a nastepnie wypisze najwieksza i najmniejsza
+void zad9()
+{
+    fstream plik;
+    plik.open("liczby.txt", ios::in);
+    int liczba;
+    int tab[100];
+    int i = 0;
+    while (plik >> liczba)
+    {
+        tab[i] = liczba;
+        i++;
+    }
+    int najwieksza = tab[0];
+    int najmniejsza = tab[0];
+    for (int i = 0; i < 100; i++)
+    {
+        if (tab[i] > najwieksza)
+        {
+            najwieksza = tab[i];
+        }
+        if (tab[i] < najmniejsza)
+        {
+            najmniejsza = tab[i];
+        }
+    }
+    cout << "Najwieksza liczba: " << najwieksza << endl;
+    cout << "Najmniejsza liczba: " << najmniejsza << endl;
     plik.close();
 }
 
