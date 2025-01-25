bc2f84abda6b558b634d34ecfc59dc2e98e46f2c
Jakub <jakuboleszczuk@protonmail.com>
2025-01-25 20:29:01 +0100

    add functions for divisors, factorial, and power calculations; update main to prompt for user input

--- cw_podstawy_progr/podstawy_pliki.cpp
+++ cw_podstawy_progr/podstawy_pliki.cpp
@@ -20,8 +20,56 @@ bool czyPierwsza(int x)
     return true;
 }
 
+int dzielniki(int x)
+{
+    int dzielnik = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        if (x % i == 0)
+        {
+            dzielnik++;
+            cout << i << " ";
+        }
+    }
+    return dzielnik;
+}
+
+int silnia(int x)
+{
+    if (x == 0)
+    {
+        return 1;
+    }
+    return x * silnia(x - 1);
+}
+
+int silniaNR(int x)
+{
+    int result = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int potega(int x)
+{
+    return x * x;
+}
 int main()
 {
-    cout << czyPierwsza(9);
+    int liczba;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+
+    // dzielniki(liczba);
+    // cout << endl;
+    cout << silnia(liczba);
+    cout << endl;
+    cout << silniaNR(liczba);
+    cout << endl;
+    cout << potega(liczba);
+    cout << endl;
     return 0;
 }
